0463f42ed2f22e51ba451a9b73886c09729377eb
DG <filomenocrespojosealonso@gmail.com>
2026-02-14 14:15:44 -0700

    correccion
    Refactor multiply function into main and enhance output format.

--- 18a_functions.dart
+++ 18a_functions.dart
@@ -1,16 +1,13 @@
-import 'dart:io';
+import 'dart:io'; //importa la librería para entrada/salida en consola (leer y escribir datos)
 
-void multiply(int numero){
-  for(int c = 0; c < 10; c++){
-    int r = 0;
-    r = numero * c;
-    stdout.write('$numero * $c = $r \n');
-  }
-}
 
-main(){
+main() {
   int n = 0;
-  stdout.write('Ingrese un número: ');
-  n = int.parse(stdin.readLineSync()!);
-  multiply(n);
+  stdout.write('Ingrese un número de la tabla de multiplicar: '); //pide ingresar un
+  n = int.parse(stdin.readLineSync()!); //lee el número ingresado y lo convierte a entero
+  for (int c = 0; c < 10; c++) { //bucle para generar la tabla de multiplicar del número ingresado
+  int r = 0;
+    r = n * c; //calcula el resultado de multiplicar el número por el contador
+    stdout.writeln('$n x $c = $r \n'); //muestra el resultado en formato de tabla de multiplicar
+  }
 }
